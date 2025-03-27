7cd937b57a4b82973f6ddd6ac3b81e731fcc1998
sukantahui <huisukanta77@gmail.com>
2025-03-27 09:59:52 +0530

    guard modified

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,13 +1,9 @@
+import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const token = localStorage.getItem('auth_token'); // Check for token
 
-  if (!token) {
-    const router = new Router(); // Create Router instance
-    router.navigate(['/noAccess']); // Redirect to home if token is missing
-    return false; // Block access
-  }
+  return localStorage.getItem('auth_token') ? true : inject(Router).createUrlTree(['/home']);
 
   return true; // Allow access
 
