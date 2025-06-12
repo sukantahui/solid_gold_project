3c0480656748168d656d0ca7a08158c6c9dcd93c
sukantahui <huisukanta77@gmail.com>
2025-06-12 11:41:13 +0530

    working on customer edit

--- src/app/pages/customer/show-customers/show-customers.component.ts
+++ src/app/pages/customer/show-customers/show-customers.component.ts
@@ -3,7 +3,12 @@ import { CustomerInterface } from '../../../interfaces/customer.interface';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import Swal from 'sweetalert2';
@@ -11,23 +16,31 @@ import { CustomerService } from '../../../services/customer.service';
 import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-show-customers',
-  imports: [CommonModule, MatIconModule,
+  imports: [
+    CommonModule,
+    MatIconModule,
     ReactiveFormsModule,
     MatButtonModule,
     MatSelectModule,
     MatFormFieldModule,
-],
+  ],
   templateUrl: './show-customers.component.html',
   styleUrl: './show-customers.component.scss',
 })
 export class ShowCustomersComponent {
-customerForm: FormGroup;
-private fb = inject(FormBuilder);
+  showEditDiv = false;
+  customerForm: FormGroup;
+  private fb = inject(FormBuilder);
   private customerService = inject(CustomerService);
-customerCategories: any;
-edit(customer: CustomerInterface) {
-  console.log(customer);
-}
+  customerCategories: any;
+  editCustomer(customer: CustomerInterface) {
+    this.showEditDiv = true;
+    console.log(customer);
+  }
+  cancelEditCustomer(){
+    this.showEditDiv = false;
+    this.customerForm.reset();
+  }
   isDevMode = isDevMode();
   customers: CustomerInterface[] = [];
   private route = inject(ActivatedRoute);
@@ -46,14 +59,10 @@ edit(customer: CustomerInterface) {
       openingGoldBalance: [0, [Validators.required, Validators.min(0)]],
       openingCashBalance: [0, [Validators.required, Validators.min(0)]],
     });
-
   }
-
 
   ngOnInit(): void {
     this.customers = this.route.snapshot.data['customerResolver'].data;
     console.log('Loaded categories:', this.customers);
-
   }
-  
 }
